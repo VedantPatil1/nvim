c3c42fe7d2a989001e21143a65d4a51475de737c
vedapati <vedapati@cisco.com>
2026-01-12 21:52:07 +0530

    Added todos support

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,58 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "ibhagwan/fzf-lua",
+  },
+  opts = {
+    keywords = {
+      TODO = { icon = " ", color = "info" },
+      FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG" } },
+      HACK = { icon = " ", color = "warning" },
+      NOTE = { icon = "󰍨 ", color = "hint" },
+      PERF = { icon = "󰅒 ", color = "warning", alt = { "OPTIMIZE" } },
+    },
+    highlight = {
+      multiline = false,
+    },
+    search = {
+      command = "rg",
+      args = {
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+      },
+      pattern = [[\b(KEYWORDS):]],
+    },
+  },
+
+  keys = {
+    {
+      "<leader>ft",
+      function()
+        require("todo-comments.fzf").todo()
+      end,
+      desc = "[F]ind [T]odos (workspace)",
+    },
+    {
+      "<leader>fT",
+      function()
+        require("todo-comments.fzf").todo({
+          cwd = vim.fn.expand("%:p:h"),
+        })
+      end,
+      desc = "[F]ind [T]odos (current file)",
+    },
+    {
+      "<leader>ff",
+      function()
+        require("todo-comments.fzf").todo({
+          keywords = { "FIX", "FIXME", "BUG" },
+        })
+      end,
+      desc = "[F]ind [F]ixmes",
+    },
+  },
+}
