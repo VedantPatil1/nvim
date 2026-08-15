32294e144e001b4b8e4b6dbd59836c629ec754d6
Vedant Patil <vedantpatil.w.1@gmail.com>
2026-08-15 22:13:36 +0530

    chore: removed reference

--- nvim-main-ref/lua/user/autocommand.lua
+++
@@ -1,8 +0,0 @@
--- Highlight copied area
-vim.api.nvim_create_autocmd('TextYankPost', {
-  group = vim.api.nvim_create_augroup('highlight_yank', { clear = true }),
-  desc = 'Hightlight when yanking (copying) text',
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
